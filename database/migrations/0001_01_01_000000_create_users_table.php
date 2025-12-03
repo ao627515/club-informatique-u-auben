cf23f7652337e072faf0df3aa7457da1467d4a74
ao627515 <ao627515@gmail.com>
2025-12-03 16:43:54 +0000

    `Updated migration to make password field nullable`

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('photo_path')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
